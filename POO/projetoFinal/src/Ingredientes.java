fc11519e49e6ce77ee50712d8037aafbe63ae0b6
João Gabriel Grandotto Viana <63874169+joaograndotto@users.noreply.github.com>
2022-04-21 10:04:02 -0300

    add o restante

---
+++ POO/projetoFinal/src/Ingredientes.java
@@ -0,0 +1,34 @@
+package ProjetoFinal;
+
+public class Ingredientes {
+    private String tipoIngred;
+    private double quantIngred;
+    private double valor;
+
+    public String getTipoIngred() {
+        return tipoIngred;
+    }
+
+    public void setTipoIngred(String tipoIngred) {
+        this.tipoIngred = tipoIngred;
+    }
+
+    public double getQuantIngred() {
+        return quantIngred;
+    }
+
+    public void setQuantIngred(int quantIngred) {
+        this.quantIngred = quantIngred;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+
+}
+
