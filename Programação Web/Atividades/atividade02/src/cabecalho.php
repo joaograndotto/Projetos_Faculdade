23dd428c3046c1cc601bfce14b964bdd3732b756
João Gabriel Grandotto Viana <63874169+joaograndotto@users.noreply.github.com>
2022-04-21 10:37:30 -0300

    Add files via upload

---
+++ Programação Web/Atividades/atividade02/src/cabecalho.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link href="../css/bootstrap.css" rel="stylesheet">
+    <link href="loja.css" rel="stylesheet">
+    <title>aula2</title>
+</head>
+<body>
+    <div class="navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <a href="index.php" class="navbar-brand">Home</a>
+
+        </div>
+        <div>
+            <ul class="nav navbar-nav">
+                <li><a href="produtoform.php">Cadastro</a></li>
+                <li><a href="sobre.php">Sobre</a></li>
+            </ul>
+        </div>
+    </div>
+    
+    </div>
+    <div class="container">
+        <div class="principal">
+            
+           
---
+++ Programação Web/Atividades/atividade02/src/index.php
@@ -0,0 +1,3 @@
+<?php include("cabecalho.php"); ?>
+<h1>Bem vindo a loja do IFG</h1>
+<?php include("rodape.php"); ?>
